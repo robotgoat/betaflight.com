f6b781b81c58c15e594c3f507739b81fd4b15b31
robotgoat <robotgoat@users.noreply.github.com>
2025-10-22 13:31:57 -0700

    Basic home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,40 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import ThemedImage from '@theme/ThemedImage';
 
 import styles from './index.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
+    // <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    //   <div className="container">
+    //     <Heading as="h1" className="hero__title">
+    //       {siteConfig.title}
+    //     </Heading>
+    //     <p className="hero__subtitle">{siteConfig.tagline}</p>
+    //     <div className={styles.buttons}>
+    //       <Link
+    //         className="button button--secondary button--lg"
+    //         to="/docs/intro">
+    //         Docusaurus Tutorial - 5min ⏱️
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </header>
+    <div className={styles.background}>
+      <ThemedImage
+        alt="Betaflight logo"
+        // className='h-fit w-fit xl:mr-12 col col--4 margin-top--xl'
+        className='margin-top--xl'
+        sources={{
+          light: useBaseUrl('/img/betaflight/logo_light.svg'),
+          dark: useBaseUrl('/img/betaflight/logo_dark.svg')
+        }}
+        />
+    </div>
   );
 }
 
